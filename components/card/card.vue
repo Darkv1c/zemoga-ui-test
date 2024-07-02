fb2799761e342561b9584b13a096b5b129233a77
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2024-07-02 01:46:12 -0500

    add percentage bar

--- components/card/card.vue
+++ components/card/card.vue
@@ -13,17 +13,28 @@ const props = defineProps<CardProps>()
 const formattedDate = computed(() => {
   return `${timeAgo.format(props.topic.lastUpdated)} in ${props.topic.category}`
 });
+
+const topicImage = `url(${props.topic.picture})`
+const positivePercentage = `${props.topic.votesPercentage.positive}%`
+const negativePercentage = `${props.topic.votesPercentage.negative}%`
 </script>
 
 <template>
-  <div :class="`${styles.container}`" :style="{ backgroundImage: `url(${topic.picture})` }">
-    <div :class="`${styles.title}`">{{ topic.name }}</div>
-    <div :class="`${styles.description}`">{{ topic.description }}</div>
-    <div :class="`${styles.time}`">{{ formattedDate }} </div>
-    <Button text="Vote Now" />
-    {{
-    topic.votesPercentage.positive }}% {{ topic.votesPercentage.negative
-    }}%
+  <div :class="styles.container">
+    <div :class="styles.infoContainer">
+      <div :class="styles.title">{{ topic.name }}</div>
+      <div :class="styles.description">{{ topic.description }}</div>
+      <div :class="styles.time">{{ formattedDate }} </div>
+      <Button text="Vote Now" />
+    </div>
+    <div :class="styles.percentageBar">
+      <span :class="styles.positive">
+        {{ topic.votesPercentage.positive }}%
+      </span>
+      <span :class="styles.negative">
+        {{ topic.votesPercentage.negative }}%
+      </span>
+    </div>
   </div>
 </template>
 
@@ -33,46 +44,77 @@ const formattedDate = computed(() => {
   flex-direction: column;
   width: 300px;
   height: 300px;
-  box-sizing: border-box;
-  padding: 36px;
-  overflow: hidden;
   background-size: cover;
   background-position: center;
+  background-image: v-bind(topicImage);
 
   & * {
     color: #FFFFFF;
   }
 
-  .title {
-    font-size: 1.875rem;
-    font-weight: 400;
-    line-height: 2.25rem;
-    margin-top: auto;
-    margin-bottom: 8px;
-    overflow: hidden;
+  .infoContainer {
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    padding: 12px 36px;
+    min-width: 100%;
+    flex: 1;
+
+    .title {
+      font-size: 1.875rem;
+      font-weight: 400;
+      line-height: 2.25rem;
+      margin-top: auto;
+      margin-bottom: 8px;
+      overflow: hidden;
+    }
+
+    .description {
+      font-size: 0.938rem;
+      font-weight: 400;
+      line-height: 1.125rem;
+      margin-bottom: 12px;
+    }
+
+    .title,
+    .description {
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      line-clamp: 2;
+      -webkit-box-orient: vertical;
+    }
+
+    .time {
+      font-size: 0.75rem;
+      font-weight: 700;
+      line-height: 0.9rem;
+      margin-bottom: 12px;
+      margin-left: auto;
+    }
   }
 
-  .description {
-    font-size: 0.938rem;
-    font-weight: 400;
-    line-height: 1.125rem;
-    margin-bottom: 12px;
-  }
+  .percentageBar {
+    display: flex;
 
-  .title,
-  .description {
-    display: -webkit-box;
-    -webkit-line-clamp: 2;
-    line-clamp: 2;
-    -webkit-box-orient: vertical;
-  }
+    .positive,
+    .negative {
+      display: flex;
+      box-sizing: border-box;
+      padding: 10px 12px;
+    }
 
-  .time {
-    font-size: 0.75rem;
-    font-weight: 700;
-    line-height: 0.9rem;
-    margin-bottom: 12px;
-    margin-left: auto;
+    .positive {
+      background-color: #3CBBB499;
+      ;
+      width: v-bind(positivePercentage);
+    }
+
+    .negative {
+      display: block;
+      text-align: right;
+      background-color: #F9AD1D99;
+      width: v-bind(negativePercentage);
+    }
   }
 }
 </style>
